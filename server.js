163bbdc64facea67c3b62d2b4218cff3198dddd6
Daroshi11260 <rytaneja@gmail.com>
2022-04-18 08:13:25 -0700

    added templating

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser')
 
 const app = express();
-app.listen(9000, () => {
-    console.log('listening to port 9000')
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json())
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+    const name = req.query.name ? req.query.name : 'new user';
+    res.render('index', {
+        user: name
+    });
+});
+
+app.post('/', (req, res) => {
+    const postData = req.body.data;
+    console.log(req.body.firstName)
+    res.send(req.body.firstName);
 })
+
+const port = 9000
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+});
